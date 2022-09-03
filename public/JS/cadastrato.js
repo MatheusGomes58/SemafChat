4e57607d7373cd70249f68870c354db6f1e1b27b
Matheus Gomes <69799035+MatheusGomes58@users.noreply.github.com>
2022-09-03 14:03:36 -0300

    [0005] - Update Linux Ubuntu

--- public/JS/cadastrato.js
+++ public/JS/cadastrato.js
@@ -1,9 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -15,36 +9,28 @@ const firebaseConfig = {
   messagingSenderId: "214897507551",
   appId: "1:214897507551:web:80c04808f956e55f81bd6a",
   measurementId: "G-YDXN20NC3H"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
-
-function Ryan(){
-    var numero = document.getElementById("lbl_number").value
-    var id;
-    var number;
-
-    const getUser = await getDocs(collection(db, "users"))
-    getUser.forEach((doc) => {
-        id: doc.id
-        number: doc.numero
-    })
-    if(number != 0){
-        console.log("User ")
-    }else{
-        try {
-            const docRef = await addDoc(collection(db, "users"), {
-              numero: numero
-            });
-            console.log("Novo usuario cadastrado", docRef.id);
-        } catch (e) {
-            console.error("Error adding document: ", e);
-        }
-    }
 }
-function teste(){
-  console.log("OLA MUNDO")
+
+firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore();
+
+
+async function Ryan(){
+  var numero = document.getElementById("lbl_number").value
+  var id;
+  var number;
+  db.collection("users").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.number}`);
+    });
+  });
+  db.collection("users").add({
+    number: numero
+  })
+  .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch((error) => {
+      console.error("Error adding document: ", error);
+  });
 }
